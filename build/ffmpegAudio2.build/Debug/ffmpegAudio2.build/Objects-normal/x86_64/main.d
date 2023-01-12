504a61cee25ceb91708283ca09071f98a8302d08
Ni Liu (nil2) <nil2@cisco.com>
2023-01-12 17:36:00 +0800

    rename

--- build/ffmpegAudio2.build/Debug/ffmpegAudio2.build/Objects-normal/x86_64/main.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/nil2/Documents/learning/ffmpegAudio2/ffmpegAudio2/main.mm \
-  /Users/nil2/Documents/learning/ffmpegAudio2/ffmpegAudio2/DecodeUtil.h \
-  /Users/nil2/Documents/learning/ffmpegAudio2/ffmpegAudio2/EncodeUtil.h
